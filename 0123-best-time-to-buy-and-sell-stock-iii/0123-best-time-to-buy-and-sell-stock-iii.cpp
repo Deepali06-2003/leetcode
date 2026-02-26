b568c72406211ee2cfd0f22253b8fc76721220c7
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-02-26 20:17:08 +0530

    Time: 471 ms (13.23%), Space: 225.6 MB (10.43%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
+int helper(vector<int>& prices ,vector<vector<vector<int>>>& dp , int i , int buy , int c ){
 
+    int n = prices.size();
+    if(i == n || c ==0)return 0;
+    
+    if(dp[i][buy][c] != -1)return dp[i][buy][c];
 
+    //buy
+    if(buy == 1){                        // 0->means not allowed to buy anything            i can buy anything
+        return dp[i][buy][c] = max(  (-prices[i]+helper(prices , dp , i+1 , 0 , c)) ,
+                                      (0+ helper(prices, dp , i+1, 1, c))  );
+    }
+    //sell
+    else{                              //i have sell it and capacity will reduce        didnot sell so can't buy
+        return dp[i][buy][c] =  max( (prices[i]+helper(prices, dp , i+1, 1, c-1)) , 
+                                    (0+ helper(prices, dp , i+1, 0 , c)) );
+    }
+}
     int maxProfit(vector<int>& prices) {
         
        int n = prices.size();
 
-       vector<vector<vector<int>>> dp (n+1 , vector<vector<int>> (2, vector<int>(3, 0) ) );
-       
-       for(int i = n-1;i>=0;i--){
-        for(int b = 0;b<=1;b++){
+       vector<vector<vector<int>>> dp (n , vector<vector<int>> (2, vector<int>(3, -1) ) );
 
-            for(int c= 1;c<=2;c++){
-                    //buy
-                    if(b == 1){                        // 0->means not allowed to buy anything            i can buy anything
-                        dp[i][b][c] = max(  -prices[i]+ dp[i+1][0][c] , 0+dp[i+1][1][c]   );
-                    }
-                    //sell
-                    else{             //i have sell it and capacity will reduce        didnot sell so can't buy
-                        dp[i][b][c] = max(  prices[i]+ dp[i+1][1][c-1] , 0+dp[i+1][0][c]   );
-                    }
-            }
-        }
-       }
-
-       return dp[0][1][2];
+       return helper(prices , dp , 0 , 1, 2);
 
     }
 };
