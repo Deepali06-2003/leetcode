a17db597f2e05ae58ee963a18b121f740ffaebd0
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-03-06 20:41:28 +0530

    Time: 11 ms (45.81%), Space: 17 MB (27.55%) - LeetHub

---
+++ 0139-word-break/0139-word-break.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+bool helper(string& s, unordered_set<string>& st,vector<int>& dp , int i){
+
+    if(i == s.size())return true;
+
+    if(dp[i] != -1)return dp[i];
+
+    for(int l = 1; l<=s.size()-i ; l++){
+
+        string x = s.substr(i,l);
+        if(st.find(x)!=st.end() && helper(s, st , dp , i+l)) return dp[i] = 1;
+    }
+    return dp[i] = 0;
+}
+
+
+    bool wordBreak(string s, vector<string>& wordDict) {
+        
+        unordered_set<string>st;
+        for(string x :wordDict)st.insert(x);
+
+        vector<int>dp(s.size() , -1);
+        return helper(s, st , dp , 0);
+    }
+};
