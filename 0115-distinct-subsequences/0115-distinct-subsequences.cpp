17715dbf63822b707ab558f9583e97bb4e72a782
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-03-07 23:21:09 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -18,7 +18,22 @@ long long solve(int i,int j,string &s,string &t,int n,int m,vector<vector<long l
     int numDistinct(string s, string t) {
         int n=s.size();
         int m=t.size();
-        vector<vector<long long>>dp(n,vector<long long>(m,-1));
-        return solve(0,0,s,t,n, m , dp);
+        vector<vector<long long>>dp(n+1,vector<long long>(m+1,0));
+
+        for(int i = 0;i<=n;i++)dp[i][0] = 1;
+
+        for(int i=1;i<=n;i++){
+            for(int j =1;j<=m;j++){
+
+                if(s[i-1] == t[j-1]){
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+
+        return dp[n][m];
     }
 };
