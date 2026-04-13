6d3ab9c44f0ec0d4a7aba3fb54870cac32d06bfb
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-04-13 23:00:03 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.cpp
+++ 0763-partition-labels/0763-partition-labels.cpp
@@ -3,25 +3,24 @@ public:
     vector<int> partitionLabels(string s) {
         
         int n = s.size();
-
-        // cal last index
-        vector<int>index(26, -1);
-        for(int i=0;i<n;i++) index[s[i]-'a'] = i;
-
         vector<int>res;
-        int i =0;
-        while(i<n){
-            int l_idx = index[s[i]-'a'];
 
-            for(int j = i+1 ; j<=l_idx ; j++){
-                // if char occur after the last index then update the l_idx
-                if( index[s[j]-'a'] > l_idx ) 
-                    l_idx = index[s[j]-'a'];
+        vector<int>mp(26, -1);
+        for(int i=0;i<n;i++)mp[s[i]-'a'] =i;
+
+        int i=0;
+        while(i<n){
+
+            int l_idx = mp[s[i]-'a'];
+
+            for(int j=i+1;j<=l_idx;j++){
+                if(mp[s[j]-'a'] > l_idx) l_idx = mp[s[j]-'a'];
             }
 
             res.push_back(l_idx-i+1);
-            i = l_idx+1;
+            i = 1+l_idx;
         }
         return res;
     }
 };
+
