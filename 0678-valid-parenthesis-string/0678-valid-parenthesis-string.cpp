3cf0429c87de4c25356e8dec8e8b0bb3090ebc01
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-04-13 23:12:51 +0530

    Time: 4 ms (6.25%), Space: 11 MB (6.31%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-
-bool helper(string& s , int& n , vector<vector<int>>& dp, int i , int c){
-
-    if(c<0) return false;
-    if(i == n) return (c==0);
-
+bool helper(string& s ,  int c, int i, vector<vector<int>>& dp){
+   
+    if(i == s.size()) return (c==0);
+    if(c < 0) return false;
     if(dp[i][c] != -1)return dp[i][c];
 
-    if(s[i] == '(') return dp[i][c] = helper(s , n , dp , i+1 , c+1);
-    else if(s[i] == ')') return dp[i][c] = helper(s , n , dp , i+1 , c-1);
+    if(s[i] =='(') return dp[i][c] = helper(s, c+1 , i+1, dp);
+    else if(s[i] == ')') return dp[i][c] =helper(s, c-1 , i+1, dp);
 
-    return dp[i][c] = (helper(s , n , dp , i+1 , c+1) || helper(s , n , dp , i+1 , c-1) || helper(s , n , dp , i+1 , c));
+    else return dp[i][c] = ( helper(s, c+1 , i+1, dp) || helper(s, c-1 , i+1, dp) || helper(s, c , i+1, dp) );
 }
+
+
     bool checkValidString(string s) {
         int n = s.size();
-        vector<vector<int>> dp(n+1 , vector<int>(n+1 , -1));
-
-        return helper(s , n, dp , 0 , 0);
+        vector<vector<int>>dp(n+1, vector<int>(n+1, -1));
+        return helper(s, 0, 0, dp);
     }
 };
