ca1a0c0a71550f5aa81da7c79670693605583d4f
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-04-22 00:53:39 +0530

    Time: 0 ms (100%), Space: 13.8 MB (96.73%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -2,20 +2,16 @@ class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
         
-
         int x=0;
-        for(int i =0;i<nums.size();i++) x = x^nums[i];
-        if(x>INT_MIN && x<INT_MAX)  x = x&(-x);
+        for(int i : nums) x = x^i;
+
+        if(x>INT_MIN && x<INT_MAX) x = x&(-x);
 
         int a=0, b=0;
-        for(int i =0;i<nums.size();i++){
 
-            if((x&nums[i]) == 0){
-                a = a^ nums[i];
-            }
-            else{
-                b = b^nums[i];
-            }
+        for(int i : nums){
+            if( (x&i) ==0) a= a^i;
+            else b= b^i;
         }
 
         return {a, b};
