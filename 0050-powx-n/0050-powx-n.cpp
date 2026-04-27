8be91ea9fc7eeae132866b8b202d284cabf66a5f
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-04-27 15:28:24 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-double helper(double x ,long long n){
-    if(n==0)return 1.0;
+double helper(double x, long long n){
+    if(n ==0)return 1;
     if(n==1)return x;
 
-    double a = helper(x , n/2);
-    if(n%2 ==0)return a*a;
-    return a*a*x;
+    double y = helper(x, n/2);
+
+    if(n%2==0) return y*y;
+    return y*y*x;
 }
     double myPow(double x, int n) {
         
-        if(n==0)return 1.0;
-        if(n==1)return x;
-
-        if(n<0){
-            long long N = n;
-            N = -N;
-            return helper(1.0/x , N);
+        if(n ==0)return 1;
+        if(n<0) {
+           // int N = -n;
+            long long N = -n;
+            return helper(1.0/x, N);
         }
-        return helper(x , n);
+        return helper(x, n);
     }
 };
