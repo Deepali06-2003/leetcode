747082315eb73f5c9ca098d00138a9faac65027d
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-05-01 10:32:23 +0530

    Time: 807 ms (27.25%), Space: 11.5 MB (39.02%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
-bool helper(vector<vector<char>>& board, string word , int n , int m , int i , int j, int s){
+
+bool helper(vector<vector<char>>& board, string word , int n , int m , int i , int j , int s){
     if(s == word.size())return true;
-    if(i<0 || i>=n || j<0 || j>=m || board[i][j]!=word[s])return false;
+
+    if(i<0 || j<0 || i>=n || j>=m || board[i][j]!=word[s])return false;
 
     char x = board[i][j];
-    board[i][j]='$';
+    board[i][j] = '#';
 
-    bool f = helper(board, word, n, m , i+1, j, s+1) || helper(board, word, n, m , i-1, j, s+1) || helper(board, word, n, m , i, j+1, s+1) || helper(board, word, n, m , i, j-1, s+1) ;
+    bool ans = helper(board , word , n , m , i+1, j , s+1) || helper(board , word , n , m , i-1, j , s+1) || helper(board , word , n , m , i, j+1 , s+1) || helper(board , word , n , m , i, j-1 , s+1);
 
-    board[i][j]= x;
-    return f;
+    board[i][j] = x;
+
+    return ans;
 }
-
     bool exist(vector<vector<char>>& board, string word) {
-        
         int n = board.size();
         int m = board[0].size();
 
-        for(int i=0;i<n;i++){
+        for(int i =0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(board[i][j] == word[0]){
-                    if(helper(board , word , n , m ,i , j, 0))return true;
+
+                if( board[i][j] == word[0]){
+                    if(helper(board , word, n , m , i , j , 0)) return true;
                 }
             }
         }
