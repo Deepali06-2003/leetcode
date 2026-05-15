b003f1a2b363ca54097b658f0155252d3b338f57
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-05-15 20:44:54 +0530

    Time: 4 ms (84.02%), Space: 12.8 MB (95.71%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -2,32 +2,27 @@ class Solution {
 public:
     string minRemoveToMakeValid(string s) {
         
-        int n = s.size();
         stack<int>st;
-        
 
-        for(int i =0;i<n;i++){
-            if(s[i] >='a' && s[i]<='z') continue;
+        for(int i=0;i<s.size() ;i++){
+
+            if(s[i]>='a' && s[i]<='z') continue;
+
             else{
 
-                if(s[i] == '(') st.push(i);
+                if(s[i]=='(') st.push(i);
                 else{
-                    if(!st.empty()) {
-                        st.pop();
-                    } else {
-                        s[i] = '*';   // mark invalid ')'
-                    }
+                    if(st.empty()) s[i] = '*';
+                    else st.pop();
                 }
             }
         }
-        
+
         while(!st.empty()){
             s[st.top()] = '*';
             st.pop();
         }
-
-        s.erase(remove(s.begin(), s.end(), '*'), s.end());
+        s.erase(remove(s.begin(), s.end(), '*') , s.end());
         return s;
-
     }
 };
