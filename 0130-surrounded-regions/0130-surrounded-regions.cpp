6eeabf9170bd26bdb8575c3c07ca0da3dd131832
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-05-18 21:23:15 +0530

    Time: 0 ms (100%), Space: 15.6 MB (15.88%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -7,40 +7,44 @@ public:
 
         queue<pair<int, int>>q;
 
-        for(int i=0;i<n;i++){
-            if(board[i][0] == 'O') q.push({i, 0});
-            if(board[i][m-1] == 'O') q.push({i, m-1});
+       for(int i=0;i<n;i++){
+        if(board[i][0] == 'O') q.push({i, 0});
+        if(board[i][m-1] == 'O') q.push({i, m-1});
+       }
+
+        for(int i=0;i<m;i++){
+        if(board[0][i] == 'O') q.push({0, i});
+        if(board[n-1][i] == 'O') q.push({n-1, i});
+       }
+
+       vector<int>dx ={0, 0, 1, -1};
+       vector<int>dy = {1, -1, 0, 0};
+
+       while(!q.empty()){
+        auto t = q.front();
+        q.pop();
+
+        if(t.first<0 || t.second<0 || t.first>=n || t.second>=m || board[t.first][t.second] != 'O')continue;
+
+        board[t.first][t.second] = '#';
+
+        for(int j =0;j<4;j++){
+            int nx = t.first + dx[j];
+            int ny = t.second + dy[j];
+
+            q.push({nx, ny});
         }
+       }
+
+       for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
-            if(board[0][j]=='O') q.push({0, j});
-            if(board[n-1][j]=='O') q.push({n-1, j});
+            if(board[i][j] == '#') board[i][j] ='O';
+            else board[i][j] ='X';
         }
+       }
 
-        vector<int> dx = {0 , 0 , 1, -1};
-        vector<int>dy  = {1, -1, 0 , 0};
-        
-        while(!q.empty()){
-            pair<int, int>temp = q.front();
-            q.pop();
 
-            if(temp.first<0 || temp.second<0 || temp.first>=n || temp.second>=m || board[temp.first][temp.second] !='O')    
-                continue;
 
-            board[temp.first][temp.second] = '#';            
-
-            for(int j=0;j<4;j++){
-                int nx = temp.first + dx[j];
-                int ny = temp.second + dy[j];
-                q.push({nx, ny});
-            }
-        }
-
-        for(int i=0;i<n;i++){
-            for(int j =0;j<m;j++){
-                if(board[i][j] == '#') board[i][j] = 'O';
-                else board[i][j] = 'X';
-            }
-        }
 
     }
 };
