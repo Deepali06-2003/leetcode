2cc34bbde5f46f80bc896c985bc45b45b1d7d4ce
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-05-20 21:48:16 +0530

    Time: 76 ms (86.59%), Space: 117.3 MB (57.69%) - LeetHub

--- 1851-minimum-interval-to-include-each-query/1851-minimum-interval-to-include-each-query.cpp
+++ 1851-minimum-interval-to-include-each-query/1851-minimum-interval-to-include-each-query.cpp
@@ -1,46 +1,29 @@
 class Solution {
 public:
     vector<int> minInterval(vector<vector<int>>& intervals, vector<int>& queries) {
-        int n = queries.size();
+        vector<int>res(queries.size() , -1);
 
-        // store {query, original_index}
-        vector<pair<int,int>> q;
-        for(int i = 0; i < n; i++){
-            q.push_back({queries[i], i});
-        }
-
-        sort(q.begin(), q.end());
         sort(intervals.begin(), intervals.end());
+        vector<pair<int, int>>query;
+        for(int i=0;i<queries.size();i++) query.push_back({queries[i], i});
 
-        vector<int> res(n, -1);
+        sort(query.begin(), query.end());
 
-        // {length, end}
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+        priority_queue<pair<int, int> , vector<pair<int, int>>, greater<pair<int, int>>>pq;
+        int j =0;
 
-        int j = 0;
+        for(int i=0;i<query.size();i++){
+            int q = query[i].first;
+            int idx = query[i].second;
 
-        for(int i = 0; i < n; i++){
-            int query = q[i].first;
-            int idx = q[i].second;
-
-            // push valid intervals
-            while(j < intervals.size() && intervals[j][0] <= query){
-                int l = intervals[j][0];
-                int r = intervals[j][1];
-                pq.push({r - l + 1, r});
+            while(j<intervals.size() && intervals[j][0] <= q){
+                pq.push({  intervals[j][1]- intervals[j][0]+1 , intervals[j][1] });
                 j++;
             }
+            while(!pq.empty() && (pq.top().second< q) ) pq.pop();
 
-            // remove invalid intervals
-            while(!pq.empty() && pq.top().second < query){
-                pq.pop();
-            }
-
-            if(!pq.empty()){
-                res[idx] = pq.top().first;
-            }
+            if(!pq.empty()) res[idx] = pq.top().first;
         }
-
         return res;
     }
 };
