cae4abded6c10cb20584c853bb9c16a84922116b
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-05-22 12:38:53 +0530

    Time: 0 ms (100%), Space: 19.3 MB (66.52%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        int v = numCourses;
-        vector<vector<int>> adj(v);
+        
+        vector<vector<int>> adj(numCourses);
+
         for(int i=0;i<prerequisites.size();i++){
             int u = prerequisites[i][0];
             int v = prerequisites[i][1];
             adj[v].push_back(u);
         }
 
-        vector<int>indegree(v, 0);
-        for(int i=0;i<v;i++){
-            for(auto j : adj[i])indegree[j]++;
+        vector<int>indegree(numCourses,0);
+        for(int i=0;i<numCourses;i++){
+            for(auto j : adj[i]){
+                indegree[j]++;
+            }
         }
 
         queue<int>q;
-        for(int i=0;i<v;i++){
-            if(indegree[i]==0) q.push(i);
+        for(int i=0 ; i<indegree.size(); i++){
+            if( indegree[i] == 0)q.push(i);
         }
 
         vector<int>res;
@@ -26,12 +29,12 @@ public:
 
             res.push_back(x);
 
-            for(auto j: adj[x]){
+            for(auto j : adj[x]){
                 indegree[j]--;
                 if(indegree[j]==0) q.push(j);
             }
         }
 
-        return (res.size() == v);
+        return (res.size()==numCourses);
     }
 };
