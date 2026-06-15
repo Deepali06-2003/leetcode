2f592b162a1e0a4752143733099031d505879079
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-06-15 19:26:20 +0530

    Time: 69 ms (13.53%), Space: 35.6 MB (11.96%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
@@ -1,34 +1,32 @@
 class Solution {
 public:
 
-int helper(vector<vector<int>>& matrix, vector<vector<int>>& dp , int n , int m , int i, int j){
+int helper(vector<vector<int>>& matrix , vector<vector<int>>& dp, int n, int m , int i , int j){
     if(dp[i][j] != 0)return dp[i][j];
 
-    int dir[4][2] = {{0,1}, {1,0}, {0,-1}, {-1,0}};
-    int c=1;
-    for(auto d: dir){
-        int x= i+d[0];
-        int y = j+d[1];
+    vector<int>dx = {0, 0, 1, -1};
+    vector<int>dy = {1, -1, 0,0};
 
-        if(x>=0 && y>=0 && x<n && y<m && matrix[x][y] > matrix[i][j]){
-            c = max(c , 1+helper(matrix, dp, n , m, x, y));
+    int c=1;
+    for(int x = 0;x<4;x++){
+        int nx = i+dx[x];
+        int ny = j+dy[x];
+
+        if(nx>=0 && ny>=0 && nx<n && ny<m && matrix[nx][ny]>matrix[i][j]){
+            c = max(c , 1+helper(matrix , dp , n, m, nx, ny));
         }
     }
-
-    dp[i][j]= c;
+    dp[i][j] = c;
     return c;
 }
     int longestIncreasingPath(vector<vector<int>>& matrix) {
         
-        int n = matrix.size(), m = matrix[0].size();
-
-        vector<vector<int>> dp(n , vector<int>(m , 0));
+        int n = matrix.size() ,m= matrix[0].size();
+        vector<vector<int>> dp(n, vector<int>(m , 0));
         int ans=0;
-
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-
-                ans = max(ans , helper(matrix , dp , n , m , i , j));
+                ans = max(ans , helper(matrix , dp , n, m, i, j));
             }
         }
         return ans;
