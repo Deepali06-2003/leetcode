4b0a973b87b5782a496aaeda4a36ad3d1e52e679
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-06-16 21:19:02 +0530

    Time: 3 ms (37.71%), Space: 14 MB (83.1%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
-void helper(vector<int>& candidates, int target,vector<vector<int>>& res, vector<int>curr, int start){
+void helper(vector<int>& candidates, int target, vector<vector<int>>& res, vector<int>& curr , int idx){
+    if(target == 0){
+        res.push_back(curr);
+        return;
+    }
+    if(target<0 || idx == candidates.size()) return;
 
-   if(target==0){
-    res.push_back(curr);
-    return;
-   }
-   if(target<0 || start == candidates.size())return;
-
-   for(int i=start;i<candidates.size();i++){
-    if(i>start && candidates[i] ==candidates[i-1])continue;
-    
-    curr.push_back(candidates[i]);
-    helper(candidates , target-candidates[i], res, curr, i+1);
-    curr.pop_back();
-   }
+    for(int i = idx ;i<candidates.size();i++){
+        if(i>idx && candidates[i] ==candidates[i-1])continue;
+        curr.push_back(candidates[i]);
+        helper(candidates, target-candidates[i] , res, curr, i+1);
+        curr.pop_back();
+    }
 }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        
+        int n = candidates.size();
+        sort(candidates.begin(), candidates.end());
         vector<vector<int>>res;
         vector<int>curr;
-        sort(candidates.begin(), candidates.end());
-        helper(candidates, target , res, curr , 0);
+
+        helper(candidates , target , res, curr, 0);
         return res;
     }
 };
