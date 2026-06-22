2ecbaf5d777b8980612e60bbf31af2bac30876a5
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-06-22 10:42:15 +0530

    Time: 0 ms (100%), Space: 7.8 MB (71.51%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -5,10 +5,22 @@ public:
         if(n==0 || n==1) return 1;
         if(n==2) return 2;
 
-        vector<int>dp(n+1, 0);
-        dp[0]=1 , dp[1]=1;
-        for(int i =2;i<=n;i++) dp[i]= dp[i-1]+dp[i-2];
+ //       vector<int>dp(n+1, 0);
+   //     dp[0]=1 , dp[1]=1;
 
-        return dp[n];
+ //dp[i]= dp[i-1]+dp[i-2];
+
+        //return dp[n];
+
+
+        int a = 1, b=1;
+        int ans= 0;
+
+        for(int i=2;i<=n;i++){
+            ans = a + b;
+            a=b;
+            b = ans;
+        }
+        return ans;
     }
 };
