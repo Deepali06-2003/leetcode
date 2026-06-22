ec41576d9886c2b74130586ed24d47fef9270c61
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-06-22 09:23:05 +0530

    Time: 0 ms (100%), Space: 11.6 MB (61.4%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        
+        int s =1;
+        for(int i = digits.size()-1;i>=0 ;i--){
+            s = s+digits[i];
+            digits[i]= s%10;
+            s=s/10;
+        }
+        if(s!=0) digits.insert(digits.begin(), s);
+        return digits;
+    }
+};
