992a205e9d4396558b6eb4c26b44d6fa27f43534
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-06-24 11:56:28 +0530

    Time: 0 ms (100%), Space: 61 MB (25.95%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
@@ -11,65 +11,40 @@
  */
 class Solution {
 public:
-void helper(TreeNode* root , vector<int>& ans){
+
+TreeNode* first_ele = NULL;
+TreeNode* second_ele = NULL;
+TreeNode* prev = NULL;
+
+void helper(TreeNode* root ){
     if(root == NULL)return;
 
-    helper(root->left, ans );
+    helper(root->left);
+    // link is broken
+    if(prev && prev->val > root->val) {
 
-    ans.push_back(root->val);
+            if(first_ele == NULL) {
+                first_ele = prev;
+                second_ele = root;
+            }
+            else {
+                second_ele = root;
+            }
+        }
 
-    helper(root->right , ans);
+        prev = root;
+
+    helper(root->right);
 }
-TreeNode* findNode(TreeNode* root , int val){
-    if(root == NULL) return NULL;
-
-    if(root->val == val) return root;
-
-    TreeNode* left = findNode(root->left, val);
-    if(left) return left;
-
-    return findNode(root->right, val);
-}
-
 
     void recoverTree(TreeNode* root) {
         
         if(root == NULL)return;
         if(root->left == NULL && root->right == NULL) return;
 
-        vector<int>ans;
-        helper(root , ans);
-
-        vector<int>temp = ans;
-        sort(temp.begin(), temp.end());
-
-        vector<int>v_swapped;
-        for(int i=0 ;i<temp.size(); i++){
-            if(temp[i] != ans[i]) v_swapped.push_back(ans[i]);
-        }
-
-        TreeNode* f_node = findNode(root , v_swapped[0]);
-        TreeNode* s_node = findNode(root , v_swapped[1]);
-        swap(f_node->val , s_node->val);
-
+        helper(root);
+        
+        swap(first_ele->val , second_ele->val);
     }
-    
-    
-
-
 };
 
-/*
-//find ele in ans which are not in sorted order
-        int prev = ans[0];
-        int i=1;
-        while( i<ans.size() && prev < ans[i]){
-            prev = ans[i];
-            i++;
-        }
-        int f_ele = prev , s_ele = ans[i];
-
-        TreeNode* f_eleA = findNode(root , f_ele);
-        TreeNode* s_eleA = findNode(root , s_ele);
-        swap(f_eleA->val , s_eleA->val)
-*/
