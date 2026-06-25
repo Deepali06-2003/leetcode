8d8ee6c2f63874f8561a34922bf6234ceec6914f
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-06-25 20:41:24 +0530

    Time: 0 ms (100%), Space: 11.4 MB (64.57%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -2,36 +2,31 @@ class Solution {
 public:
     string minWindow(string s, string t) {
         
-        vector<int>freq(256, 0);
         int n = s.size() , m = t.size();
-        if(m> n)return "";
+        if(m > n) return "";
 
-        for(int i =0;i<m;i++) freq[t[i]]++;
+        vector<int>freq(256, 0);
+        for(char c : t) freq[c]++;
 
-        int l =0 , r=0;
-        int s_i =-1, min_len = INT_MAX;
-        int c=0;
-
+        int c=0, s_i = -1 , len = INT_MAX;
+        int l=0, r=0;
         while(r<n){
-            
-            if(freq[s[r]]>0)c++;
+            if(freq[s[r]]> 0)c++;
             freq[s[r]]--;
             r++;
 
             while(c==m){
-                if((r-l)<min_len){
-                    min_len = r-l;
+                if((r-l) < len){
+                    len = r-l;
                     s_i = l;
                 }
+
                 freq[s[l]]++;
-                if(freq[s[l]]> 0) c--;
+                if(freq[s[l]] > 0)c--;
                 l++;
             }
         }
-
-        if(s_i==-1)return "";
-        return s.substr(s_i, min_len);
-              
-
+        if(s_i == -1) return "";
+        return s.substr(s_i , len);
     }
 };
