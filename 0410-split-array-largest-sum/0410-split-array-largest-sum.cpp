1a94077db6d0a9302b5570d76bf20ddd73eb8a15
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-07-02 23:29:25 +0530

    Time: 0 ms (100%), Space: 10.6 MB (42.56%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,35 +1,34 @@
 class Solution {
 public:
-int helper(vector<int>& nums, int k, int m){
-    int books =1, curr=0;
+int helper(vector<int>& nums, int capacity){
+    int s =1;
+    int curr_s=0;
 
-    for(int i=0;i<nums.size();i++){
-
-        if((curr+nums[i])<=m){
-            curr = curr+nums[i];
+    for(int i : nums){
+        if((curr_s+i)> capacity){
+            s++;
+            curr_s = i;
         }
         else{
-            books++;
-            curr = nums[i];
+            curr_s = curr_s + i;
         }
-    }
-    return books;
+    }return s;
 }
     int splitArray(vector<int>& nums, int k) {
         
         int n = nums.size();
-        
+        if(k > n) return -1;
 
-        int l = *max_element(nums.begin(), nums.end());
-        int h = accumulate(nums.begin() , nums.end(), 0);
+        long long l = *max_element(nums.begin(), nums.end());
+        long long h = accumulate(nums.begin(), nums.end(), 0);
 
         while(l<=h){
-            int m = (l+h)/2;
+            long long m = (l+h)/2;
 
-            int ans = helper(nums, k , m);
-            
-            if(ans > k)l = m+1;
-            else{ h = m-1;}
+            int ans = helper(nums, m);
+
+            if(ans > k) l = m+1;
+            else h = m-1;
         }
         return l;
     }
