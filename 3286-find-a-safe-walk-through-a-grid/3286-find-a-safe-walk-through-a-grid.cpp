d2b9b072d0a026a493d9fcc9deeb0f665e17d35a
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-07-03 10:42:39 +0530

    Time: 35 ms (34.24%), Space: 37.3 MB (28.66%) - LeetHub

--- 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
+++ 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
@@ -4,37 +4,37 @@ public:
         
         int n = grid.size(), m = grid[0].size();
 
-        int startHealth = health - grid[0][0];
-        if(startHealth < 1) return false;
+        queue<tuple<int, int, int>>q;
+        vector<vector<int>>best_h(n , vector<int>(m, -1));
 
-        queue<tuple<int,int,int>> q;
-        q.push({0, 0, startHealth});
+        health = health - grid[0][0];
+        if(health < 1) return false;
 
-        vector<vector<int>> best(n, vector<int>(m, -1));
-        best[0][0] = startHealth;
+        q.push({0, 0, health});
+        best_h[0][0] = health;
 
         vector<int>dx = {0, 0, 1, -1};
         vector<int>dy = {1, -1, 0, 0};
-
         while(!q.empty()){
+
             auto [x, y, h] = q.front();
             q.pop();
 
-            if(x == n-1 && y== m-1 && h>=1) return true;
-
-            for(int j = 0;j<4;j++){
-                int nx = x + dx[j];
-                int ny = y + dy[j];
+            if(x == (n-1) && y ==(m-1)) return true;
+            for(int j =0;j<4;j++){
+                int nx = x+dx[j];
+                int ny = y+dy[j];
                 int nh = h;
 
-                if(nx>=0 && ny>=0 && nx<n && ny<m ){
-                    nh = nh - grid[nx][ny];
+                if(nx>=0 && nx<n && ny>=0 && ny<m){
+                    nh = nh-grid[nx][ny];
+
                     if(nh<1)continue;
-                    //updating health if new health
-                    if(nh > best[nx][ny]) {
-                        best[nx][ny] = nh;
+                    if(nh > best_h[nx][ny]){
                         q.push({nx, ny, nh});
+                        best_h[nx][ny] = nh;
                     }
+
                 }
             }
         }
