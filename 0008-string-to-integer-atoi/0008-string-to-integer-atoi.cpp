fcc1966eb4723b511f6aee18554738eff72340a3
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-07-05 01:00:33 +0530

    Time: 0 ms (100%), Space: 9.3 MB (21.62%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -6,28 +6,27 @@ public:
         int i = 0;
         int n = s.size();
 
-        //space
         while(i<n && s[i]==' ') i++;
 
-        //sign
-        int sign = 1;
-        if(i<n && (s[i]=='-' || s[i]=='+') ){
-            if(s[i] == '-')sign = -1;
+        int sign =1;
+        if(i<n && (s[i]=='-' || s[i]=='+')){
+            if(s[i]=='-') sign = -1;
             i++;
         }
-
 
         int ans=0;
+
         while(i<n && (s[i]>='0' && s[i]<='9')){
-            int dig = int(s[i]-'0');
 
-            if(ans > INT_MAX/10 || (ans == INT_MAX/10 && dig > 7)) return sign == 1 ? INT_MAX : INT_MIN;
-
-            ans = ans*10 + dig;
+            int d = s[i]-'0';
+            if(ans > INT_MAX/10 || (ans==INT_MAX/10 && d >7)){
+                if(sign == -1) return INT_MIN;
+                return INT_MAX;
+            }
+            ans = ans*10 +d;
             i++;
         }
 
-        if(sign == -1)return -1*ans;
-        return ans;
+        return sign*ans;
     }
 };
