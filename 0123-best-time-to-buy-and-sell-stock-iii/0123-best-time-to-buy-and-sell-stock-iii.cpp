335dd33bb0d5d6efbb4ed901de2f4590d56c3dc7
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-07-05 00:31:07 +0530

    Time: 395 ms (49.14%), Space: 211.4 MB (45.71%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,30 +1,21 @@
 class Solution {
 public:
-
-
     int maxProfit(vector<int>& prices) {
+        
+        int n = prices.size();
 
-       int n = prices.size();
+        vector<vector<vector<int>>> dp (n+1, vector<vector<int>>(2, vector<int>(3, 0)));
 
-       vector<vector<vector<int>>> dp (n+1 , vector<vector<int>> (2, vector<int>(3, 0))) ;
-       
-       for(int i=n-1 ; i>=0 ; i--){
-            for(int b=0 ; b<=1 ; b++){
+        for(int i =n-1;i>=0 ;i--){
+            for(int b = 0;b<=1;b++){
+                for(int k = 1;k<=2;k++){
 
-                for(int c=1 ; c<=2 ; c++){
-                    //buy
-                    if(b == 1){                       
-                         // 0->means not allowed to buy anything        i can buy anything
-                        dp[i][b][c] = max(  -prices[i]+ dp[i+1][0][c] , 0+dp[i+1][1][c]   );
-                    }
-                    //sell
-                    else{           
-                          //i have sell it and capacity will reduce   didnot sell so can't buy
-                        dp[i][b][c] = max(  prices[i]+ dp[i+1][1][c-1] , 0+dp[i+1][0][c]   );
-                    }
+                    if(b==1) dp[i][b][k] = max(-prices[i] + dp[i+1][0][k] , dp[i+1][1][k]);
+                    else dp[i][b][k] = max(prices[i]+dp[i+1][1][k-1] , dp[i+1][0][k]);
                 }
             }
-       }
-       return dp[0][1][2];
+        }
+
+        return dp[0][1][2];
     }
 };
