9b4b6870055e11735a596f2c0e74aa90438c8bdd
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-07-06 23:01:51 +0530

    Time: 0 ms (100%), Space: 71.6 MB (80.91%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -3,12 +3,13 @@ public:
     int maxSubArray(vector<int>& nums) {
         
         int n = nums.size();
-        int ans= INT_MIN;
-        int curr=0;
-        for(int i=0;i<n;i++){
-            curr = curr+nums[i];
-            if(curr>ans) ans = curr;
-            if(curr<0) curr=0;
+
+        int ans = INT_MIN, s=0;
+
+        for(int i: nums){
+            s = s+i;
+            ans = max(ans , s);
+            if(s < 0) s=0;
         }
         return ans;
     }
