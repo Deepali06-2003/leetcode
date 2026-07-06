7576b327ae6a7ce22698cc958c7076b545660d0a
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-07-06 10:21:30 +0530

    Time: 82 ms (67.14%), Space: 117.2 MB (83.79%) - LeetHub

--- 1851-minimum-interval-to-include-each-query/1851-minimum-interval-to-include-each-query.cpp
+++ 1851-minimum-interval-to-include-each-query/1851-minimum-interval-to-include-each-query.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
     vector<int> minInterval(vector<vector<int>>& intervals, vector<int>& queries) {
-        vector<int>res(queries.size() , -1);
+        int n = intervals.size();
 
-        sort(intervals.begin(), intervals.end());
         vector<pair<int, int>>query;
-        for(int i=0;i<queries.size();i++) query.push_back({queries[i], i});
+        for(int i=0;i<queries.size();i++)   query.push_back({queries[i], i});
 
         sort(query.begin(), query.end());
+        sort(intervals.begin(), intervals.end());
 
         priority_queue<pair<int, int> , vector<pair<int, int>>, greater<pair<int, int>>>pq;
-        int j =0;
 
+        vector<int>res(queries.size(), -1);
+        int j=0;
         for(int i=0;i<query.size();i++){
             int q = query[i].first;
             int idx = query[i].second;
 
-            while(j<intervals.size() && intervals[j][0] <= q){
-                pq.push({  intervals[j][1]- intervals[j][0]+1 , intervals[j][1] });
+            while(j<n && intervals[j][0]<=q){
+                pq.push({ intervals[j][1]-intervals[j][0]+1 , intervals[j][1] });
                 j++;
             }
-            while(!pq.empty() && (pq.top().second< q) ) pq.pop();
+
+            while(!pq.empty() && pq.top().second < q) pq.pop();
 
             if(!pq.empty()) res[idx] = pq.top().first;
         }
+
         return res;
     }
 };
