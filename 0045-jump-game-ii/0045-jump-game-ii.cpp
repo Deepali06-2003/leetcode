555cb248f5a67edf8c837730ba26e2006a306253
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-07-06 23:46:31 +0530

    Time: 0 ms (100%), Space: 20.5 MB (58.49%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
     int jump(vector<int>& nums) {
         
-        int jump =0, stop=0;
-        int max_l =0;
+        int j = 0, stop = 0;
+        int max_i = 0;
 
         for(int i=0;i<nums.size()-1;i++){
+            max_i = max(max_i, nums[i]+i);
 
-            max_l = max(max_l , nums[i]+i);
-
-            if(i == stop){
-                jump++;
-                stop = max_l;
+            if(stop == i){
+                stop = max_i;
+                j++;
             }
         }
-        return jump;
+
+        return j;
     }
 };
