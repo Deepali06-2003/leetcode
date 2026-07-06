2a2e077602dcae0772feef279429d81604fb6608
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-07-06 23:44:32 +0530

    Time: 0 ms (100%), Space: 52.3 MB (83.65%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-       int max_i =0;
+        int max_i = 0;
+        
+        for(int i=0;i<nums.size();i++){
+            if(i > max_i) return false;
+            max_i = max(max_i, nums[i]+i);
+        }
 
-       for(int i =0;i<nums.size();i++){
-        if(i>max_i)return false;
-
-        max_i = max(max_i , nums[i]+i);
-
-       }
-       return true;
+        return true;
     }
 };
