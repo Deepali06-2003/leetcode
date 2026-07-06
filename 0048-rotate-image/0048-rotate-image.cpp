a2d8afacb9277f14ce978d1b90c685ccec659028
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-07-06 22:54:44 +0530

    Time: 0 ms (100%), Space: 10.2 MB (69.91%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -4,18 +4,16 @@ public:
         
         int n = matrix.size();
 
-        vector<vector<int>> dp(n,vector<int>(n, 0));
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
 
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                dp[j][n-i-1] = matrix[i][j];
-            }
+            reverse(matrix[i].begin(), matrix[i].end());
         }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                matrix[i][j] = dp[i][j];
-            }
-        }
+        
         
     }
 };
