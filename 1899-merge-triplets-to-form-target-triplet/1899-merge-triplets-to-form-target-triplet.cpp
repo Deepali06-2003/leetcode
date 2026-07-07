0cba7de0a51ea0ec46ccc624373231bd5bdc21cb
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-07-07 09:52:23 +0530

    Time: 8 ms (33.55%), Space: 150.6 MB (67.41%) - LeetHub

--- 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.cpp
+++ 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     bool mergeTriplets(vector<vector<int>>& triplets, vector<int>& target) {
-        int a=0, b=0, c=0;
+       
+        int a = 0, b=0, c=0;
 
         for(int i =0;i<triplets.size();i++){
-            if(triplets[i][0]> target[0] || triplets[i][1]>target[1] || triplets[i][2]>target[2]) continue;
-
+            if( (triplets[i][0]> target[0]) || (triplets[i][1]> target[1]) || (triplets[i][2]> target[2]) ) continue;
             a = max(a, triplets[i][0]);
-            b= max(b, triplets[i][1]);
-            c = max(c, triplets[i][2]);
+            b = max(b , triplets[i][1]);
+            c= max(c, triplets[i][2]);
         }
 
         return ((a == target[0]) && (b==target[1]) && (c==target[2]));
-
     }
 };
