6bec3036259064c6ce1f74d83246367311c7ec9b
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-07-07 09:31:14 +0530

    Time: 136 ms (10.73%), Space: 55.2 MB (5.84%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
 
-bool helper(string& s, string& p,vector<vector<int>>& dp, int n , int m ,int i, int j ){
-    if(j==m){
-        return i==n;
-    }
-    if(dp[i][j]!=-1)return dp[i][j];
+bool helper(string& s, string& p , int n , int m , int i , int j, vector<vector<int>>& dp){
+    if(j == m) return (i==n);
+
+    if(dp[i][j] != -1) return dp[i][j];
+
+    bool f_char = false;
+    if((i<n) && (s[i]==p[j] || p[j]=='?')) f_char = true;
 
     if(j<m && p[j]=='*'){
-        bool take = (i<n) && helper(s,p,dp, n , m, i+1, j);
-        bool n_take = helper(s, p, dp,n, m , i, j+1);
-        return dp[i][j] = take||n_take;
-    }
-    
-    bool f_char = ( (i<n) && ((s[i]==p[j]) || (p[j]=='?')) );
-    return dp[i][j] = f_char && helper(s,p, dp,n, m, i+1,j+1);
-    
+        bool t = (i<n) && helper(s, p, n, m, i + 1, j, dp);
+        bool nt = helper(s, p, n, m, i, j + 1, dp);
 
+        return dp[i][j] = t || nt;
+    }
+    return dp[i][j] = f_char && helper(s, p, n, m, i+1, j+1, dp);
 }
     bool isMatch(string s, string p) {
         
-        int n = s.size(), m =p.size();
-        vector<vector<int>>dp(n+1, vector<int>(m+1, -1));
-        return helper(s, p , dp , n , m , 0 , 0);
+        int n = s.size() , m = p.size();
+
+        vector<vector<int>> dp(n+1, vector<int>(m+1 , -1));
+        return helper(s, p, n, m , 0, 0, dp);
     }
 };
