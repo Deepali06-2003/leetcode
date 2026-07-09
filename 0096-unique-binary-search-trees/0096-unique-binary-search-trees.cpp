4f28f39f63489c8003429134fa797013cff3e07c
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-07-09 21:08:14 +0530

    Time: 0 ms (100%), Space: 7.9 MB (61.84%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     int numTrees(int n) {
-        if(n ==0 || n==1) return 1;
+        
+        if(n<=0) return 0;
+        if(n==1) return 1;
 
         vector<int>dp(n+1, 0);
-        dp[0]=1, dp[1] =1;
+        dp[1]=1;
+        dp[0]=1;
 
-        for(int i = 2 ;i<=n; i++){
-            for(int root = 1;root<=i ;root++){
-                dp[i] = dp[i] + ( dp[root-1] * dp[i-root] );
-                cout<< i<<" "<<root<< " "<< dp[i]<<endl;
+        for(int i =2;i<=n;i++){
+            for(int root=1; root<=i; root++){
+
+                dp[i] = dp[i] + (dp[root-1]*dp[i-root]);
             }
-            
         }
         return dp[n];
     }
