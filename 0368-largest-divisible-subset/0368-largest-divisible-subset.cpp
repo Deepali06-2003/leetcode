4c5a6aa69aa9b8d8814caafcefedb0817c79340d
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-07-09 15:54:13 +0530

    Time: 11 ms (63.02%), Space: 12.7 MB (51.14%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
@@ -1,39 +1,34 @@
 class Solution {
 public:
-
-
     vector<int> largestDivisibleSubset(vector<int>& nums) {
         
-        int n = nums.size();
-        sort(nums.begin(),nums.end());
+        sort(nums.begin(), nums.end());
 
+        int n = nums.size();
+        vector<int>prev(n, -1);
         vector<int>dp(n , 1);
-        vector<int>prev_idx(n, -1);
-        int max_len = 1, last_idx=0;
+        int max_len = 1, last_idx =0;
 
         for(int i=0;i<n;i++){
             for(int j=0;j<i;j++){
-                if(nums[i]%nums[j] == 0){
-                    // updating dp[i] if it is smaller
-                    if(dp[i] < (dp[j]+1) ){
-                        dp[i] = dp[j]+1;
-                        prev_idx[i] = j;
+                if(nums[i] % nums[j] == 0){
+
+                    if(dp[j]+1 > dp[i]){
+                        dp[i]= dp[j]+1;
+                        prev[i] = j;
                     }
-                    // updating max_length and last index having longest substring
-                    if(max_len < dp[i]){
+
+                    if(dp[i] > max_len){
+                        max_len = dp[i];
                         last_idx = i;
-                        max_len = max(max_len , dp[i]);
                     }
                 }
-                                
             }
         }
-
-        vector<int>ans;
+        vector<int>res;
         while(last_idx != -1){
-            ans.push_back(nums[last_idx]);
-            last_idx = prev_idx[last_idx];
-        }
-        return ans;
+            res.push_back(nums[last_idx]);
+            last_idx = prev[last_idx];
+        }return res;
     }
 };
