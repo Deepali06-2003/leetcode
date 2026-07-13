e1aed3496fed8492e8ee1cea4b6ffac986672f61
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-07-13 09:37:00 +0530

    Time: 2 ms (31.42%), Space: 20.9 MB (29.84%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -5,24 +5,23 @@ public:
         int n = matrix.size();
         int m = matrix[0].size();
 
-        vector<int>ROW(n, 0);
-        vector<int>COL(m, 0);
+        vector<int>dx(n, 1);
+        vector<int>dy(m , 1);
 
         for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+            for(int j =0;j<m;j++){
                 if(matrix[i][j] == 0){
-                    ROW[i] = 1;
-                    COL[j] = 1;
+                    dx[i]=0;
+                    dy[j]=0;
                 }
             }
         }
         for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(ROW[i] == 1 || COL[j]==1){
+            for(int j =0;j<m;j++){
+                if(dx[i]==0 || dy[j]==0){
                     matrix[i][j]=0;
                 }
             }
         }
-        
     }
 };
