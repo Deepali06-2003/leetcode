5da77a14cbc5d14f6552a537f259db81397cc60c
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-07-13 09:48:01 +0530

    Time: 0 ms (100%), Space: 9.9 MB (9.38%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,32 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        
-        int n = numRows;
-        vector<vector<int>> dp(n, vector<int>(n, 0));
+    int n = numRows;
 
-        for(int i =0;i<n;i++) dp[i][0] =1;
+    vector<vector<int>> dp(numRows, vector<int>(numRows, 0));
+    vector<vector<int>>res;
+    vector<int>temp;
 
-        for(int i =1;i<n;i++){
-            for(int j = 1;j<n;j++){
 
-                if(i == j) dp[i][j] =1;
-                else dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
-            }
+    for(int i=0;i<n;i++)dp[i][0]=1;
+    res.push_back({1});
+
+    for(int i =1;i<n;i++){
+        temp.clear();
+        temp.push_back(1);
+
+        for(int j=1;j<=i; j++){
+
+            if(i == j) dp[i][j] = 1;
+            else dp[i][j] = dp[i-1][j] + dp[i-1][j-1];
+            temp.push_back(dp[i][j]);
         }
+        res.push_back(temp);
+    }
 
-        vector<vector<int>>res;
-        vector<int>curr;
 
-        for(int i=0;i<n;i++){
-            curr.clear();
-
-            for(int j=0;j<n;j++){
-                if(dp[i][j] != 0) curr.push_back(dp[i][j]);
-            }
-            res.push_back(curr);
-        }
-
-        return res;
+    return res;
     }
 };
