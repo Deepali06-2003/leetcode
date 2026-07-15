de72688995cc1b601f3c0cc66df2e034d88aff54
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-07-15 12:09:01 +0530

    Time: 17 ms (29.93%), Space: 14.3 MB (20.24%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -6,18 +6,16 @@ public:
         unordered_set<char>st;
         int ans=0;
 
-        int l = 0, r=0;
+        int l=0, r=0;
         while(r<n){
-            while( st.find(s[r]) != st.end() ){
+            while(st.find(s[r]) != st.end()){
                 st.erase(s[l]);
                 l++;
             }
+            ans = max(ans, r-l+1);
             st.insert(s[r]);
-            ans = max(ans , r-l+1);
             r++;
-
         }
-
         return ans;
     }
 };
