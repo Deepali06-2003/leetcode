971602e2a814fbe3ea59c5ed4f19ff878e7696df
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-07-18 23:57:10 +0530

    Time: 2 ms (44.66%), Space: 14 MB (84.88%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
-void helper(vector<int>& candidates, int target, vector<vector<int>>& res, vector<int>& curr , int idx){
+void helper(vector<int>& candidates, int target, vector<vector<int>>& res, vector<int>& curr ,int n, int idx){
     if(target == 0){
         res.push_back(curr);
         return;
     }
-    if(target<0 || idx == candidates.size()) return;
 
-    for(int i = idx ;i<candidates.size();i++){
-        if(i>idx && candidates[i] ==candidates[i-1])continue;
+    if(idx == n || target < 0) return;
+
+    for(int i = idx ;i<n ; i++){
+        if(i>idx && candidates[i] == candidates[i-1]) continue;
         curr.push_back(candidates[i]);
-        helper(candidates, target-candidates[i] , res, curr, i+1);
+        helper(candidates , target-candidates[i] , res, curr , n , i+1);
         curr.pop_back();
     }
 }
@@ -21,7 +22,7 @@ void helper(vector<int>& candidates, int target, vector<vector<int>>& res, vecto
         vector<vector<int>>res;
         vector<int>curr;
 
-        helper(candidates , target , res, curr, 0);
+        helper(candidates , target , res, curr,n, 0);
         return res;
     }
 };
