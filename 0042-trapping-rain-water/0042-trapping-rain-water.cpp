b2f3e341458575eca2f713757bdf503a29257302
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-07-18 10:44:24 +0530

    Time: 3 ms (16.72%), Space: 27.4 MB (6.82%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -3,25 +3,20 @@ public:
     int trap(vector<int>& height) {
         int n = height.size();
 
-        vector<int>l(n, 0);
-        vector<int>r(n, 0);
+        vector<int>prefix(n , 0);
+        vector<int>postfix(n , 0);
 
-        l[0] = height[0];
-        r[n-1] = height[n-1];
+        prefix[0] = height[0];
+        postfix[n-1] = height[n-1];
 
-        for(int i=1;i<n;i++){
-            l[i] = max(l[i-1], height[i]);
-        }
+        for(int i=1;i<n;i++) prefix[i] = max(prefix[i-1], height[i]);
+        for(int i=n-2;i>=0;i--) postfix[i] = max(postfix[i+1], height[i]);
 
-        for(int i=n-2;i>=0;i--){
-            r[i] = max(r[i+1] , height[i]);
-        }
-
-        int ans = 0;
+        int ans=0;
         for(int i=0;i<n;i++){
-            ans = ans + (min(l[i], r[i]) - height[i]);
+            ans =  ans+ (min(prefix[i], postfix[i]) - height[i]);
         }
-
         return ans;
+
     }
 };
