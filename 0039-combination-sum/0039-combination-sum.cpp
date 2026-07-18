da80361d50c049ee66b763bcaecdc3c50e66d003
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-07-18 23:44:13 +0530

    Time: 140 ms (5.09%), Space: 64.9 MB (8.82%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,24 +1,28 @@
 class Solution {
 public:
 
-void helper(vector<int>& candidates, int target, vector<vector<int>>&res, vector<int> curr, int idx){
-
+void helper(vector<int>& candidates, int target, vector<vector<int>>& res, vector<int> curr, int n , int idx){
     if(0 == target){
         res.push_back(curr);
         return;
     }
-    if(target<0 || idx==candidates.size()) return;
-     
+
+    if(idx == n || target < 0) return;
+
     curr.push_back(candidates[idx]);
-    helper(candidates, target-candidates[idx] , res, curr, idx);
+    helper(candidates, target-candidates[idx] , res, curr, n , idx);
     curr.pop_back();
 
-    helper(candidates, target , res, curr, idx+1);
+    helper(candidates, target , res, curr, n , idx+1);
 }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        int n = candidates.size();
+        sort(candidates.begin(), candidates.end());
+
         vector<vector<int>>res;
         vector<int>curr;
-        helper(candidates, target , res, curr, 0);
+
+        helper(candidates, target, res, curr, n , 0);
         return res;
     }
 };
