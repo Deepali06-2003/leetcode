25308ddf6315d4ad410f9be7c2b3cb43b6334ce6
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-07-20 23:43:05 +0530

    Time: 27 ms (34.89%), Space: 37.3 MB (20.18%) - LeetHub

--- 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
+++ 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
@@ -2,39 +2,39 @@ class Solution {
 public:
     bool findSafeWalk(vector<vector<int>>& grid, int health) {
         
-        int n = grid.size(), m = grid[0].size();
+        int n = grid.size();
+        int m = grid[0].size();
+
+        int h = health;
+        h = h - grid[0][0];
+
+        if(h<1) return false;
 
         queue<tuple<int, int, int>>q;
-        vector<vector<int>>best_h(n , vector<int>(m, -1));
-
-        health = health - grid[0][0];
-        if(health < 1) return false;
-
-        q.push({0, 0, health});
-        best_h[0][0] = health;
-
+        vector<vector<int>> best_h(n, vector<int>(m , 0));
         vector<int>dx = {0, 0, 1, -1};
         vector<int>dy = {1, -1, 0, 0};
-        while(!q.empty()){
 
-            auto [x, y, h] = q.front();
+        q.push({0, 0, h});
+        best_h[0][0] = h;
+
+        while(!q.empty()){
+            auto[x, y, z] = q.front();
             q.pop();
 
-            if(x == (n-1) && y ==(m-1)) return true;
-            for(int j =0;j<4;j++){
-                int nx = x+dx[j];
-                int ny = y+dy[j];
-                int nh = h;
+            if(x == n-1 && y == m-1) return true;
+            
+            for(int j=0;j<4;j++){
+                int nx = x+ dx[j];
+                int ny = y + dy[j];
 
-                if(nx>=0 && nx<n && ny>=0 && ny<m){
-                    nh = nh-grid[nx][ny];
+                if(nx>=0 && ny>=0 && nx<n && ny<m){
+                    int nh = z - grid[nx][ny];
 
-                    if(nh<1)continue;
                     if(nh > best_h[nx][ny]){
-                        q.push({nx, ny, nh});
                         best_h[nx][ny] = nh;
+                        q.push({nx, ny, nh});
                     }
-
                 }
             }
         }
