e670e83a6ec0472253e0ba20c995286b7746563d
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-08-15 23:34:10 +0530

    Time: 142 ms (42.9%), Space: 205.7 MB (6.13%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -10,20 +10,16 @@ public:
 
         int target = s/2;
 
-        vector<vector<unsigned long long>> dp(n+1, vector<unsigned long long>(target+1, 0));
+        vector<vector<unsigned long long>>dp(n+1, vector<unsigned long long>(target+1, 0));
 
-        for(int i =0;i<=n;i++) dp[i][0]=1;
-
+        for(int i=0;i<=n;i++) dp[i][0]=1;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=target;j++){
 
                 if(nums[i-1] > j) dp[i][j] = dp[i-1][j];
-                else dp[i][j] = dp[i-1][j] + dp[i-1][j- nums[i-1]];
+                else dp[i][j] = dp[i-1][j] + dp[i-1][j-nums[i-1]];
             }
         }
-
         return dp[n][target];
-
-
     }
 };
