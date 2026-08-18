a6571f2723732f8e6536d9709696849826479a52
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-08-18 21:11:45 +0530

    Time: 59 ms (33.9%), Space: 101.9 MB (87.54%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.cpp
+++ 0853-car-fleet/0853-car-fleet.cpp
@@ -3,24 +3,24 @@ public:
     int carFleet(int target, vector<int>& position, vector<int>& speed) {
         
         int n = position.size();
+        vector<pair<int, int>>arr(n);
+        for(int i=0;i<n;i++){
+            arr[i] = {position[i] , speed[i]};
+        }
 
-        vector<pair<int, int>>arr;
-        for(int i=0;i<n;i++)arr.push_back({position[i] , speed[i] });
+        sort(arr.rbegin(), arr.rend());
 
-        sort(arr.rbegin() , arr.rend());
+        double curr = (double)(target-arr[0].first)/arr[0].second;
+        int ans=1;
 
-        double ans = (double)(target-arr[0].first)/ arr[0].second;
-        int c=1;
-        double x;
+        for(int i=1;i<n;i++){
+            double x = (double)(target-arr[i].first)/arr[i].second;
 
-        for(int i =1;i<n;i++){
-            x = (double)(target-arr[i].first)/ arr[i].second;
-            if(x > ans){
-                c++;
-                ans = x;
+            if(x>curr){
+                ans++;
+                curr = x;
             }
         }
-        
-        return c;
+        return ans;
     }
 };
