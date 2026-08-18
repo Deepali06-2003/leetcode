b71dcc20ccd23dfb780a4d94f5ac064a0e9fd7fc
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-08-18 21:31:43 +0530

    Time: 30 ms (42.74%), Space: 47.6 MB (38.84%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,36 +1,37 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        int N = tasks.size();
         vector<int>freq(26, 0);
-
-        for(int i : tasks) freq[i-'A']++;
+        for(char c : tasks){
+            freq[c-'A'] +=1;
+        }
 
         priority_queue<int>pq;
-        for(int i : freq){
-            if( i != 0 )pq.push(i);
+        for(int i=0;i<26;i++){
+            if(freq[i]>0) pq.push(freq[i]);
         }
-        int ans=0;
-        while(!pq.empty()){
-           
-            vector<int>curr;
-            for(int i=0;i<=n; i++){
-                if(!pq.empty()){
-                    int f = pq.top();
-                    pq.pop();
 
-                    curr.push_back(f-1);
+        int ans=0;
+
+        while(!pq.empty()){
+            vector<int>curr;
+            for(int i=0;i<=n;i++){
+
+                if(!pq.empty()){
+                int f = pq.top();
+                pq.pop();
+
+                curr.push_back(f-1);
                 }
             }
 
-            for(int i : curr){
-                if(i > 0) pq.push(i);
+            for(int i=0;i<curr.size();i++){
+                if(curr[i]>0) pq.push(curr[i]);
             }
 
-            if(pq.empty()) ans = ans + curr.size();
-            else ans = ans+ n+1;
+            if(pq.empty()) ans = ans+curr.size();
+            else ans = ans+n+1;
         }
-
         return ans;
     }
 };
