25b7bb73848c2d6c8225fd544df399f54fbb3afd
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-08-19 11:25:53 +0530

    Time: 214 ms (10.95%), Space: 13.9 MB (61.98%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
 
-int helper(vector<int>& nums, int l , int h, vector<vector<int>>& dp){
+int helper(vector<int>& nums, int l, int r, vector<vector<int>>& dp){
+    if(l>r) return 0;
+    if(dp[l][r] != -1) return dp[l][r];
 
-    if(l>h)return 0;
-    if(dp[l][h] != -1) return dp[l][h];
     int ans = INT_MIN;
+    for(int x=l;x<=r;x++){
+        int curr = nums[x] * nums[l-1] * nums[r+1];
+        curr = curr + helper(nums, l, x-1, dp) + helper(nums, x+1, r, dp);
 
-    for(int i=l;i<=h;i++){
-        int x = nums[l-1] * nums[i] * nums[h+1];
-        x = x+ helper(nums, l, i-1, dp) + helper(nums, i+1, h, dp);
-        ans = max(ans, x);
+        ans = max(ans, curr);
     }
-    return dp[l][h] = ans;
+    return dp[l][r]=ans;
 }
     int maxCoins(vector<int>& nums) {
         
         int n = nums.size();
-
-        nums.insert(nums.begin(), 1);
         nums.push_back(1);
-        
-        vector<vector<int>> dp (n+1, vector<int>(n+1, -1));
+        nums.insert(nums.begin(), 1);
+
+        vector<vector<int>>dp(n+1, vector<int>(n+1, -1));
         return helper(nums, 1, n, dp);
     }
 };
