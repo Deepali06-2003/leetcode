11a914a839f6707e6767aeb59eb5cc09be78f8ac
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-08-21 20:34:11 +0530

    Time: 3 ms (40.93%), Space: 12.6 MB (76.4%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,32 +1,22 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums2.size();
-
-        vector<int>arr(n, -1);
+        unordered_map<int, int>mp;  //value , next grater
         stack<int>st;
-
-        //find next greater element
-        for(int i =0;i<n;i++){
-            while(!st.empty() && nums2[i]>nums2[st.top()]){
-                arr[st.top()] = nums2[i];
+        for(int i=0;i<nums2.size();i++){
+            while(!st.empty() && nums2[i]> st.top()){
+                mp[st.top()] = nums2[i];
                 st.pop();
             }
-            st.push(i);
+            st.push(nums2[i]);
         }
 
-        // to keep check of index 
-        unordered_map<int,int> mp;
-        for(int i = 0; i < n; i++){
-            mp[nums2[i]] = i;
+        for(int i=0;i<nums1.size();i++){
+            int x = nums1[i];
+            if(mp.find(x) != mp.end()) nums1[i] = mp[x];
+            else nums1[i]=-1;
         }
+        return nums1;
 
-        vector<int>res;
-        for(int i : nums1){
-            
-            res.push_back(arr[mp[i]]);
-        }
-
-        return res;
     }
 };
