54651b738bdd76eaa79ad278b8b883235712b978
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-08-24 19:07:01 +0530

    Time: 11 ms (14.95%), Space: 23.9 MB (5.11%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -1,27 +1,32 @@
 class Solution {
 public:
 
-bool helper(string& s, string& p, vector<vector<int>>& dp, int n , int m, int i, int j){
+bool helper(string s, string p, int n, int m , vector<vector<int>>& dp, int i, int j){
+    if(j==m) 
+        return (i==n);
+    if(dp[i][j]!=-1) return dp[i][j];
 
-    if(j == m) return (i==n);
-    if(dp[i][j] != -1) return dp[i][j];
-    
     bool f_char = false;
-    if(i<n && ( (s[i] == p[j]) || (p[j]=='.') ) ) f_char = true;
+    if( i<n &&( p[j]=='.' || s[i]==p[j]) ) f_char = true;
 
-    if( ((j+1)< m) && (p[j+1]== '*') ){
-        bool take = f_char && helper(s, p, dp, n, m, i+1, j);
-        bool n_take = helper(s,p,dp, n, m, i,j+2);
-
-        return dp[i][j] = take || n_take;
+    if((j+1)<m && p[j+1]=='*'){
+        bool take = f_char &&  helper(s, p, n, m, dp, i+1, j);
+        bool not_take = helper(s, p, n,m, dp, i, j+2);
+        return dp[i][j] = take || not_take;
     }
-    return dp[i][j] = f_char && helper(s, p, dp, n, m, i+1, j+1);
+
+    return dp[i][j] = f_char && helper(s, p, n, m , dp, i+1, j+1);
 }
     bool isMatch(string s, string p) {
         
-        int n = s.size() , m = p.size();
-        vector<vector<int>> dp (n+1, vector<int>(m+1, -1));
+        int n = s.size() , m =p.size();
 
-        return helper(s, p, dp, n, m, 0, 0);
+        vector<vector<int>>dp(n+1, vector<int>(m+1, -1));
+
+        return helper(s, p, n, m , dp, 0, 0);
+        //return dp[n][m];
     }
 };
+
+
+    
