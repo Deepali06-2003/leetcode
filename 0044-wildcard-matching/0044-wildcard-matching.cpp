4f2708888b588bb162352ecd97e8b3143c850fc7
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-08-24 19:20:05 +0530

    Time: 148 ms (7.68%), Space: 55 MB (6.05%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
 
-bool helper(string& s, string& p , int n , int m , int i , int j, vector<vector<int>>& dp){
-    if(j == m) return (i==n);
-
-    if(dp[i][j] != -1) return dp[i][j];
-
-    bool f_char = false;
-    if((i<n) && (s[i]==p[j] || p[j]=='?')) f_char = true;
-
-    if(j<m && p[j]=='*'){
-        bool t = (i<n) && helper(s, p, n, m, i + 1, j, dp);
-        bool nt = helper(s, p, n, m, i, j + 1, dp);
-
-        return dp[i][j] = t || nt;
-    }
-    return dp[i][j] = f_char && helper(s, p, n, m, i+1, j+1, dp);
-}
-    bool isMatch(string s, string p) {
+bool helper(const string& s, const string& p, int n , int m, vector<vector<int>>& dp, int i, int j){
+        if(j==m)
+            return (i==n);
         
+        if(dp[i][j] != -1) return dp[i][j];
+
+        bool f_char = false;
+        if(i<n && (p[j]=='?' || s[i]==p[j] ) ) f_char = true;
+
+        if(j<m && p[j]=='*'){
+            bool t = (i<n) && helper(s, p, n , m , dp, i+1, j);
+            bool ntake = helper(s, p, n , m , dp, i, j+1);
+
+            return dp[i][j] = t || ntake;
+        }
+        return  dp[i][j] = f_char && helper(s, p, n,m, dp, i+1, j+1);
+}
+
+    bool isMatch(string s, string p) {
         int n = s.size() , m = p.size();
 
-        vector<vector<int>> dp(n+1, vector<int>(m+1 , -1));
-        return helper(s, p, n, m , 0, 0, dp);
+        vector<vector<int>>dp(n+1, vector<int>(m+1 , -1));
+        return helper(s, p, n, m, dp, 0, 0);
     }
 };
