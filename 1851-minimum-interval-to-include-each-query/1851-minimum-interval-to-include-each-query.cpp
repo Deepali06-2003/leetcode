289810b5eaa3100b10b019ba3e01b7ffcafecdf0
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-08-24 14:33:49 +0530

    Time: 83 ms (66.27%), Space: 117.1 MB (81.48%) - LeetHub

--- 1851-minimum-interval-to-include-each-query/1851-minimum-interval-to-include-each-query.cpp
+++ 1851-minimum-interval-to-include-each-query/1851-minimum-interval-to-include-each-query.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
     vector<int> minInterval(vector<vector<int>>& intervals, vector<int>& queries) {
-        int n = intervals.size();
+        int n = queries.size();
+     vector<pair<int, int>>query;
+     for(int i=0;i<n;i++){
+        query.push_back({queries[i], i});
+     }  
+     sort(query.begin(), query.end());
+     sort(intervals.begin(), intervals.end());
 
-        vector<pair<int, int>>query;
-        for(int i=0;i<queries.size();i++)   query.push_back({queries[i], i});
+     vector<int>res(n, -1); 
+     priority_queue<pair<int, int> ,vector<pair<int, int>>, greater<pair<int, int>>>pq;
+        //pair<int, int>
 
-        sort(query.begin(), query.end());
-        sort(intervals.begin(), intervals.end());
+            int j=0;
+     for(int i=0;i<n;i++){
+        int q = query[i].first;
+        int x = query[i].second;
 
-        priority_queue<pair<int, int> , vector<pair<int, int>>, greater<pair<int, int>>>pq;
-
-        vector<int>res(queries.size(), -1);
-        int j=0;
-        for(int i=0;i<query.size();i++){
-            int q = query[i].first;
-            int idx = query[i].second;
-
-            while(j<n && intervals[j][0]<=q){
-                pq.push({ intervals[j][1]-intervals[j][0]+1 , intervals[j][1] });
-                j++;
-            }
-
-            while(!pq.empty() && pq.top().second < q) pq.pop();
-
-            if(!pq.empty()) res[idx] = pq.top().first;
+        while(j<intervals.size() && intervals[j][0]<=q){ 
+            pq.push({intervals[j][1]-intervals[j][0]+1 , intervals[j][1]});
+            j++;
         }
+        while(!pq.empty() && pq.top().second < q) pq.pop();
 
-        return res;
+        if(!pq.empty()) res[x]=pq.top().first;
+     }
+     return res;
     }
 };
