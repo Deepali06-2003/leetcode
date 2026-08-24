0d74dbc82e283f1f1f6580df72078e03b6e87653
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-08-24 21:16:55 +0530

    Time: 0 ms (100%), Space: 15.7 MB (82.81%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -6,26 +6,23 @@ public:
         int n = nums.size();
 
         int cand_idx = -1;
-        for(int i = n-1;i>0;i--){
-            if(nums[i-1] < nums[i]){
+        for(int i=n-1;i>0;i--){
+            if(nums[i-1]<nums[i]){
                 cand_idx = i-1;
                 break;
             }
         }
 
         if(cand_idx != -1){
-
-            int right_grt = cand_idx;
-            for(int i=n-1 ; i>cand_idx ;i--){
-                if(nums[i] > nums[cand_idx]){
-                    right_grt = i;
-                    break;
-                }
-            }
-
-            swap(nums[cand_idx] , nums[right_grt]);
+        int r_idx = cand_idx;
+        for(int i=n-1;i>cand_idx;i--){
+            if(nums[i]>nums[cand_idx]){ 
+                r_idx = i;
+                break;}
         }
 
-        reverse(nums.begin()+cand_idx+1 , nums.end());
+        swap(nums[cand_idx], nums[r_idx]);}
+
+        reverse(nums.begin()+cand_idx+1, nums.end());
     }
 };
