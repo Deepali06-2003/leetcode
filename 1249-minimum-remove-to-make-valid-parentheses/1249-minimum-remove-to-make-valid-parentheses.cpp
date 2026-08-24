5b289b14144f6dc60784e1484ec19f7b34199f12
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-08-24 20:16:17 +0530

    Time: 0 ms (100%), Space: 12.8 MB (97.15%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -3,24 +3,21 @@ public:
     string minRemoveToMakeValid(string s) {
         
         stack<int>st;
-
         for(int i=0;i<s.size();i++){
-            if(s[i] =='(' || s[i]==')'){
-                if(s[i]=='(')    st.push(i);
-
-                else{
-                    if(st.empty()) s[i]='#';
-                    else st.pop();
-                }
+            if(s[i]>='a'&& s[i]<='z')continue;
+            else if(s[i]=='(') st.push(i);
+            else{
+                if(!st.empty()) st.pop();
+                else s[i]='#';
             }
         }
 
         while(!st.empty()){
-            s[st.top()] = '#';
+            s[st.top()]='#';
             st.pop();
         }
 
-        s.erase( remove(s.begin(), s.end(), '#'), s.end());
+        s.erase( remove(s.begin(), s.end(), '#') , s.end());
         return s;
     }
 };
