1117cb2922d88db11654e42698696dcacab0918f
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-08-27 19:05:51 +0530

    Time: 16 ms (80.99%), Space: 81.3 MB (79.54%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -4,30 +4,33 @@ public:
         
         int n = heights.size();
         int ans = 0;
-
         stack<int>st;
+
         for(int i=0;i<n;i++){
-            while(!st.empty() && heights[i] < heights[st.top()] ){
-                int ele = st.top();
+            while(!st.empty() && heights[i] < heights[st.top()]){
+                int x = st.top();
+
                 st.pop();
 
-                int nse = i;
-                int pse = (st.empty())? -1 : st.top();
+                int nxs = i;
+                int pvs = (st.empty())?-1 : st.top();
 
-                ans = max(ans , (nse-pse-1)*heights[ele]);
+                ans = max(ans , ((nxs-pvs-1)*heights[x]));
             }
             st.push(i);
         }
-
         while(!st.empty()){
-                int ele = st.top();
+                int x = st.top();
+
                 st.pop();
 
-                int nse = n;
-                int pse = (st.empty())? -1 : st.top();
+                int nxs = n;
+                int pvs = (st.empty())?-1 : st.top();
 
-                ans = max(ans , (nse-pse-1)*heights[ele]);
-        }
+                ans = max(ans , ((nxs-pvs-1)*heights[x]));
+            }
+
         return ans;
+
     }
 };
