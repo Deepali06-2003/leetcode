9abecf608e245abd604006663c7062d2bff7f2d2
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-08-29 19:00:11 +0530

    Time: 3 ms (62.44%), Space: 17.9 MB (65.32%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -14,29 +14,28 @@ public:
         queue<pair<int, pair<int, int>>>q;
         vector<int>dist(n, INT_MAX);
 
-        q.push({0, {src,0}});
+        q.push({0, {src, 0}});
         dist[src]=0;
 
         while(!q.empty()){
             auto t = q.front();
             q.pop();
 
-            int stop = t.first , node = t.second.first , c_d = t.second.second;;
+            int stop = t.first;
+            int n = t.second.first;
+            int d = t.second.second;
 
             if(stop > k) continue;
 
-            for(auto j : adj[node]){
-                int n = j.first;
-                int d = j.second;
-
-                if(c_d+d < dist[n] && stop+1 <=k+1){
-                    dist[n] = c_d+d;
-                    q.push({stop+1 , {n, dist[n]}});
+            for(auto j : adj[n]){
+                if((j.second + d < dist[j.first]) && (k+1 >= stop+1)){
+                    dist[j.first] = j.second + d;
+                    q.push({stop+1, {j.first, dist[j.first]} });
                 }
             }
         }
+
         if(dist[dst] == INT_MAX) return -1;
         return dist[dst];
-
     }
 };
