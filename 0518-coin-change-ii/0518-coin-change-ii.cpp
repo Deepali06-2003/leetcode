b278e7330fac243bb476a525a18bcda26b53d3d9
Deepali Saroha <deepalisaroha2003@gmail.com>
2026-09-16 00:37:33 +0530

    Time: 35 ms (42.55%), Space: 61.7 MB (5.77%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -10,11 +10,12 @@ public:
 
         for(int i=1;i<=n;i++){
             for(int j=1;j<=amount;j++){
-
                 if(coins[i-1]>j) dp[i][j] = dp[i-1][j];
-                else dp[i][j] = dp[i-1][j] + dp[i][j-coins[i-1]];
+                else dp[i][j] = dp[i-1][j]+ dp[i][j-coins[i-1]];
             }
         }
+        
+        
         return dp[n][amount];
     }
 };
